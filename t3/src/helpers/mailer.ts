5c1a3aa549163119d2010863934cb0250093c8d2
olos23 <140463521+olos23@users.noreply.github.com>
2023-07-26 15:19:57 -0400

    Update mailer.ts

--- t3/src/helpers/mailer.ts
+++ t3/src/helpers/mailer.ts
@@ -27,7 +27,7 @@ export const sendEmail = async({email, emailType, userId}:any) => {
 
 
         const mailOptions = {
-            from: 'hassimzafar@gmail.com',
+            from: '',
             to: email,
             subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
             html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
